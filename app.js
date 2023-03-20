374e90689f20ae8bd9d472348d111be32416fd78
Thien Nguyen <tatthien.contact@gmail.com>
2023-03-20 15:44:23 +0700

    feat: allow using custom port via env

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.listen(3000, () => {
-  console.log('Server is running at http://localhost:3000')
-})
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
